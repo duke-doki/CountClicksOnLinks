4d0b31b31e51e757b1876f4d475c029dbe7be719
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-10-04 19:06:32 +0300

    Adding files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Bitly URL shortener
+
+This project checks if typed URL is already a bitly-link or not. If it is, user will see an
+amount of clicks done on this link, if it is not, then user will see a shortened bitly-link variant of the typed link.
+
+### How to install
+
+The token can be accessed at https://app.bitly.com/settings/api/ after registration.
+Token must be in `Bearer {token}` format. It must be stored as header in the URL request. For more information: https://dev.bitly.com/.
+
+Python3 should already be installed. 
+Use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Project Goals
+
+This code was written for educational purposes as part of an online course for web developers at [dvmn.org](https://dvmn.org/).
---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+from urllib.parse import urlparse
+
+import requests
+import argparse
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def shorten_link(token, long_url):
+    headers = {'Authorization': f'Bearer {token}'}
+    url = 'https://api-ssl.bitly.com/v4/bitlinks'
+    response = requests.post(
+        url, 
+        headers=headers, 
+        json={"long_url": long_url}
+    )
+    response.raise_for_status()
+    bitlink = response.json()['link']
+    return bitlink
+
+
+def count_clicks(token, bitlink):
+    bitlink = urlparse(bitlink)
+    headers = {'Authorization': f'Bearer {token}'}
+    link = ("https://api-ssl.bitly.com/v4/"
+            "bitlinks/{}{}/clicks/summary").format(
+                bitlink.netloc, 
+                bitlink.path
+            )
+    response = requests.get(link, headers=headers)
+    response.raise_for_status()
+    clicks = response.json()['total_clicks']
+    return clicks
+
+
+def is_bitlink(token, url_to_check):
+    url_to_check = urlparse(url_to_check)
+    headers = {'Authorization': f'Bearer {token}'}
+    link = ("https://api-ssl.bitly.com/"
+        "v4/bitlinks/{}{}").format(url_to_check.netloc, url_to_check.path)
+    response = requests.get(link, headers=headers)
+    return response.ok
+
+
+def main():
+    bitly_token = os.environ['BITLY_TOKEN']
+    parser = argparse.ArgumentParser()
+    parser.add_argument('link', help='Enter a link')
+    args = parser.parse_args()
+    link_to_check = args.link
+    try:
+        if is_bitlink(bitly_token, link_to_check):
+            print('Количество кликов: ', count_clicks(bitly_token, link_to_check))
+        else:
+            print('Битлинк: ', shorten_link(bitly_token, link_to_check))
+    except requests.exceptions.HTTPError:
+        print('Неправильная ссылка')
+
+
+if __name__ == '__main__':
+    main()
